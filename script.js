35e011a6d86d2213d32c030d3530eba1e7e90013
TIGER KILLER <choprakhushil13@gmail.com>
2025-03-20 13:21:45 +0530

    updated

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+const jwt = require('jsonwebtoken');
 const encrypt = (payload, secret) => {
-  // your code here and return token
+  return jwt.sign(payload, secret, { expiresIn: '1h' });
 };
 
 module.exports = encrypt;
